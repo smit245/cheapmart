ffe62d2c14dccc90c5d77754734b9940ecd9f92e
smit245 <49567481+smit245@users.noreply.github.com>
2022-05-30 13:14:24 +0530

    Initial Commit
    Commiting First Time

---
+++ CheapMart/src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,69 @@
+package com.example.demo.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToMany(mappedBy = "category",fetch=FetchType.LAZY)
+	private Set<SubCategory> subcategory= new HashSet<>();
+	
+	@Column(nullable = false,length=100)
+	private String name;
+	
+	@Column(nullable = false,length=100)
+	private String image;
+	
+	public Set<SubCategory> getSubcategory() {
+		return subcategory;
+	}
+
+	public void setSubcategory(Set<SubCategory> subcategory) {
+		this.subcategory = subcategory;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Column(nullable = false,columnDefinition = "integer default 0")
+	private int status;
+}
