9dc1ada7fe2331b268b393ae76c6dd68d808a743
smit245 <49567481+smit245@users.noreply.github.com>
2022-06-26 15:56:57 +0530

    Categories
    categories shown in header section

--- CheapMart/src/main/java/com/example/demo/web/AppController.java
+++ CheapMart/src/main/java/com/example/demo/web/AppController.java
@@ -1,27 +1,38 @@
 package com.example.demo.web;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
 import com.example.demo.web.dto.UserRegistrationDto;
 
 
 @Controller
 public class AppController {
 	
+	@Autowired
+	CategoryService categoryService;
+	
 	@ModelAttribute("user")
 	public UserRegistrationDto userRegistrationDto() {
 		return new UserRegistrationDto();
 	}
 	
+	@ModelAttribute("categories")
+	public List<Category> categories(){
+		return categoryService.getAllCategories();
+	}
 	
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
